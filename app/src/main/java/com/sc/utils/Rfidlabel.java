bc7fb51b1b289f1251e1de2b5cc623d027afe620
suchun00 <1909245874@qq.com>
2017-08-17 08:52:08 +0800

    实现Gson数据传输的类

---
+++ app/src/main/java/com/sc/utils/Rfidlabel.java
@@ -0,0 +1,43 @@
+package com.sc.utils;
+
+/**
+ * Created by suchun on 2017/8/15.
+ */
+public class Rfidlabel {
+    private String phynum;
+    private String rfidid;
+    private String type;
+    private String date;
+
+    public String getPhynum() {
+        return phynum;
+    }
+
+    public void setPhynum(String phynum) {
+        this.phynum = phynum;
+    }
+
+    public String getRfidid() {
+        return rfidid;
+    }
+
+    public void setRfidid(String rfidid) {
+        this.rfidid = rfidid;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
